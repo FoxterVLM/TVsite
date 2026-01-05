6e1493441248378251384eee11e8e789063574e7
FoxterVLM <123764914+FoxterVLM@users.noreply.github.com>
2026-01-05 21:46:10 +0200

    planned open movie block

--- src/stores/useGetMovieSwiper.ts
+++ src/stores/useGetMovieSwiper.ts
@@ -1,21 +1,14 @@
 import {create} from "zustand";
 import axios from "axios";
 
-type dataType = {
-    array: string[];
-    genre: string;
-};
-
 type Props = {
-    movieData: dataType[] | null;
     getData: () => Promise<void>,
 }
 
 export const useGetMovie = create<Props>(() => ({
-    movieData: [],
     getData: async () => {
         try{
-           const response = await axios.get('/api/data.json') // add server api, where u can get data
+           const response = await axios.get('/api/swiper_db.json') // add server api, where u can get data
                .then(response => response.data)
             return response;
         }
