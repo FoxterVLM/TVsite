ca010b8db6b31999441e32e87993296dc166c73f
Annakristal14 <pro100anyshka@gmail.com>
2026-01-07 18:26:56 +0100

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Support from './pages/Support'
 
 function App() {
   const [] = useState(0)
 
   return (
     <>
-      
+    
+      <div>
+        <Support />
+      </div>
     </>
   )
 }
